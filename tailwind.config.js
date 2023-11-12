4393b36723968b965424cd695a7b39b04d308a7a
Jay Delisle <46854415+j-delisle@users.noreply.github.com>
2023-11-11 20:21:53 -0500

    initial commit with initial site layout done

---
+++ tailwind.config.js
@@ -0,0 +1,17 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+	content: ['./src/**/*.{astro,html,js,jsx,md,mdx,svelte,ts,tsx,vue}'],
+	theme: {
+		colors: {
+			'prime': '#14b8a6',
+			'prime-text': '#d4d4d8',
+			'secondary-text': '#a1a1aa',
+			'base': '#18181b',
+			'base-accent': '#1d1d21',
+			'base-light': '#3f3f46',
+			'hover': '#202022'
+		},
+		extend: {},
+	},
+plugins: [],
+}
