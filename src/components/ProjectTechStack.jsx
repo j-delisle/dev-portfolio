0a8b9f1586ccd0935f465d564274f010183160dd
Jay Delisle <46854415+j-delisle@users.noreply.github.com>
2023-11-16 21:19:33 -0500

    final touches to project card styling

--- src/components/ProjectTechStack.jsx
+++
@@ -1,8 +0,0 @@
-import { Icon } from 'astro-icon';
-
-export default function ProjectTechStack({ stack }) {
-  const tech = stack.map((x) => (
-    <Icon pack="logos" name={x.name} class="h-6 w-6" />
-  ));
-  return <ul>{tech}</ul>;
-}
