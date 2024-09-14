aaa1b6231b0a6189af8ebdc6026df13d5c144356
Jay Delisle <46854415+j-delisle@users.noreply.github.com>
2024-09-14 14:23:01 -0400

    update astro config with site

--- astro.config.mjs
+++ astro.config.mjs
@@ -7,6 +7,6 @@ import react from "@astrojs/react";
 
 // https://astro.build/config
 export default defineConfig({
-  site: 'https://example.com',
+  site: 'https://jaydelisle.dev',
   integrations: [mdx(), sitemap(), tailwind(), react()]
 });
